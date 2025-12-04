4eeee30d2a3eeb0c5950e8ba8f63263a4b084d72
whisperWDN <747618283@qq.com>
2025-12-04 23:03:18 +0800

    建立paxos项目

---
+++ paxos/src/main/java/com/whisper/paxos/PaxosApplication.java
@@ -0,0 +1,13 @@
+package com.whisper.paxos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaxosApplication.class, args);
+	}
+
+}
